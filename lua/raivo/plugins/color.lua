634d23aa60025b6d600bfbdfb5ed627c5efd8954
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-08-07 13:47:46 +0300

    update

--- lua/raivo/plugins/color.lua
+++ lua/raivo/plugins/color.lua
@@ -1,12 +1,39 @@
 return {
-	"rockerBOO/boo-colorscheme-nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.g.boo_colorscheme_italic = false
-	end,
-
-	init = function()
-		vim.cmd.colorscheme("radioactive_waste")
-	end
+	{
+		"rockerBOO/boo-colorscheme-nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.boo_colorscheme_italic = false
+		end,
+	},
+	{
+		"loctvl842/monokai-pro.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent_background = true,
+			background_clear = {
+				"toggleterm",
+				"telescope",
+				"renamer",
+				"notify",
+			},
+			styles = {
+				comment = { italic = true },
+				keyword = { italic = false }, -- any other keyword
+				type = { italic = false }, -- (preferred) int, long, char, etc
+				storageclass = { italic = false }, -- static, register, volatile, etc
+				structure = { italic = false }, -- struct, union, enum, etc
+				parameter = { italic = false }, -- parameter pass in function
+				annotation = { italic = false },
+				tag_attribute = { italic = false }, -- attribute of tag in reactjs
+			},
+		},
+		config = function(_, opts)
+			require("monokai-pro").setup(opts)
+			vim.cmd.colorscheme("monokai-pro-octagon")
+			vim.cmd.colorscheme("radioactive_waste")
+		end
+	},
 }
