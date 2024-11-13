f3da61d05a64cffe8c337a8d29ddd80c66311fbb
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2024-11-13 10:00:51 +0200

    update

--- lua/raivo/plugins/ai.lua
+++ lua/raivo/plugins/ai.lua
@@ -1,24 +1,26 @@
 return {
-	"supermaven-inc/supermaven-nvim",
-	opts = {
-		keymaps = {
-			accept_suggestion = "<Tab>",
-			clear_suggestion = "<C-]>",
-			accept_word = "<C-j>",
-		},
-		ignore_filetypes = { cpp = true }, -- or { "cpp", }
-		color = {
-			suggestion_color = "#ffffff",
-			cterm = 244,
-		},
-		log_level = "info",            -- set to "off" to disable logging completely
-		disable_inline_completion = false, -- disables inline completion for use with cmp
-		disable_keymaps = false,       -- disables built in keymaps for more manual control
-		condition = function()
-			return false
-		end
-	},
-	config = function(_, opts)
-		require("supermaven-nvim").setup(opts)
-	end,
+	'Exafunction/codeium.vim',
+	build = ":Codeium Auth",
+	config = function()
+		vim.keymap.set('i', '<C-g>',
+			function()
+				return vim.fn['codeium#Accept']()
+			end,
+			{ expr = true, silent = true })
+		vim.keymap.set('i', '<c-;>',
+			function()
+				return vim.fn['codeium#CycleCompletions'](1)
+			end,
+			{ expr = true, silent = true })
+		vim.keymap.set('i', '<c-,>',
+			function()
+				return vim.fn['codeium#CycleCompletions'](-1)
+			end,
+			{ expr = true, silent = true })
+		vim.keymap.set('i', '<c-x>',
+			function()
+				return vim.fn['codeium#Clear']()
+			end,
+			{ expr = true, silent = true })
+	end
 }
