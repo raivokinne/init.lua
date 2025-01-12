d6fc47d88baef829904ee4f6b7d6ca839556d292
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2025-01-12 23:26:21 +0200

    update

--- lua/raivo/plugins/color.lua
+++ lua/raivo/plugins/color.lua
@@ -1,27 +1,142 @@
 return {
   {
-    "zaldih/themery.nvim",
-    lazy = false,
+    "vague2k/huez.nvim",
+    -- if you want registry related features, uncomment this
+    -- import = "huez-manager.import"
+    branch = "stable",
+    event = "UIEnter",
     config = function()
-      require("themery").setup {
-        themes = {
-          "rose-pine",
-          "colorbuddy",
-          "gruvbuddy",
-          "radioactive_waste"
-        },
-        livePreview = true,
-      }
-      vim.keymap.set("n", "<leader>th", vim.cmd.Themery)
+      require("huez").setup({})
+      vim.keymap.set("n", "<leader>th", ":Huez<CR>")
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
       vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
   {
-    "rockerBOO/boo-colorscheme-nvim"
+    "cdmill/neomodern.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("neomodern").setup({
+        transparent = true,
+      })
+    end,
   },
   {
-    "tjdevries/colorbuddy.nvim",
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('github-theme').setup({
+        options = {
+          transparent = true
+        }
+      })
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      term_colors = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = { },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+    end,
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "moon",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    config = function(_, opts)
+      require("tokyonight").setup(opts)
+    end,
+  },
+  {
+    "vague2k/vague.nvim",
+    config = function()
+      require("vague").setup({
+        transparent = false, -- don't set background
+        style = {
+          -- "none" is the same thing as default. But "italic" and "bold" are also valid options
+          boolean = "none",
+          number = "none",
+          float = "none",
+          error = "none",
+          comments = "italic",
+          conditionals = "none",
+          functions = "none",
+          headings = "none",
+          operators = "none",
+          strings = "none",
+          variables = "none",
+
+          -- keywords
+          keywords = "none",
+          keyword_return = "none",
+          keywords_loop = "none",
+          keywords_label = "none",
+          keywords_exception = "none",
+
+          -- builtin
+          builtin_constants = "none",
+          builtin_functions = "none",
+          builtin_types = "none",
+          builtin_variables = "none",
+        },
+        -- Override colors
+        colors = {
+          bg = "#18191a",
+          fg = "#cdcdcd",
+          floatBorder = "#878787",
+          line = "#282830",
+          comment = "#646477",
+          builtin = "#bad1ce",
+          func = "#be8c8c",
+          string = "#deb896",
+          number = "#d2a374",
+          property = "#c7c7d4",
+          constant = "#b4b4ce",
+          parameter = "#b9a3ba",
+          visual = "#363738",
+          error = "#d2788c",
+          warning = "#e6be8c",
+          hint = "#8ca0dc",
+          operator = "#96a3b2",
+          keyword = "#7894ab",
+          type = "#a1b3b9",
+          search = "#465362",
+          plus = "#8faf77",
+          delta = "#e6be8c",
+        },
+      })
+    end,
   },
   {
     "rose-pine/nvim",
@@ -42,6 +157,7 @@ return {
     },
     config = function(_, opts)
       require("rose-pine").setup(opts)
+      vim.cmd.colorscheme("rose-pine")
     end,
   },
 }
