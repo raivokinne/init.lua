6a08fc4609e8ad38eb60fd2b88acebe6caef0f16
RaivoKinne <120594466+raivokinne@users.noreply.github.com>
2025-01-24 13:31:26 +0200

    update

--- lua/raivo/plugins/color.lua
+++ lua/raivo/plugins/color.lua
@@ -1,71 +1,50 @@
 return {
-	"vague2k/vague.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("vague").setup({
-			transparent = true,
-			style = {
-				-- "none" is the same thing as default. But "italic" and "bold" are also valid options
-				boolean = "none",
-				number = "none",
-				float = "none",
-				error = "none",
-				comments = "none",
-				conditionals = "none",
-				functions = "none",
-				headings = "bold",
-				operators = "none",
-				strings = "none",
-				variables = "none",
+	{
+		"vague2k/huez.nvim",
+		-- if you want registry related features, uncomment this
+		-- import = "huez-manager.import"
+		branch = "stable",
+		event = "UIEnter",
+		config = function()
+			require("huez").setup({})
+			vim.keymap.set("n", "<leader>th", ":Huez<CR>")
+			vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+			vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+				transparent = true, -- Enable this to disable setting the background color
+				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = false },
+					keywords = { italic = false },
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "transparent", -- style for sidebars, see below
+					floats = "transparent", -- style for floating windows
+				},
+			})
+		end
+	},
 
-				-- keywords
-				keywords = "none",
-				keyword_return = "none",
-				keywords_loop = "none",
-				keywords_label = "none",
-				keywords_exception = "none",
-
-				-- builtin
-				builtin_constants = "none",
-				builtin_functions = "none",
-				builtin_types = "none",
-				builtin_variables = "none",
-			},
-			colors = {
-				bg = "#18191a",
-				fg = "#cfe2f3",
-				floatBorder = "#878787",
-				line = "#282830",
-				comment = "#646477",
-				func = "#9fc5e8",
-				-- func = "#8e7cc3",
-				-- keyword = "#787bab",
-				builtin = "#bad1ce",
-				keyword = "#c9f2ba",
-				-- string = "#e69138",
-				-- string = "#8a739a",
-				-- string = "#f2e6ff",
-				-- number = "#f2e6ff",
-				string = "#d8d5b1",
-				property = "#eeeeee",
-				constant = "#b4b4ce",
-				parameter = "#b2d8d8",
-				visual = "#363738",
-				error = "#d2788c",
-				warning = "#e6be8c",
-				hint = "#8ca0dc",
-				operator = "#96a3b2",
-				number = "#e6e6fa",
-				type = "#8ed2cc",
-				search = "#465362",
-				plus = "#8faf77",
-				delta = "#e6be8c",
-			},
-		})
-	end,
-
-	init = function()
-		vim.cmd.colorscheme("vague")
-	end
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require('rose-pine').setup({
+				styles = {
+					italic = false,
+					bold = false,
+					transparency = true,
+				},
+			})
+		end
+	},
 }
--- lua/raivo/plugins/extra.lua
+++ lua/raivo/plugins/extra.lua
@@ -1,13 +1,4 @@
 return {
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		}
-	},
 	"jwalton512/vim-blade",
 	"nvim-tree/nvim-web-devicons",
 	-- comments
@@ -15,62 +6,5 @@ return {
 		"numToStr/Comment.nvim",
 		opts = {},
 		lazy = false,
-	},
-	-- Neovim plugin to improve the default vim.ui interfaces
-	{
-		"stevearc/dressing.nvim",
-		dependencies = { "MunifTanjim/nui.nvim" },
-		opts = {},
-		config = function()
-			require("dressing").setup()
-		end,
-	},
-
-	-- Neovim notifications and LSP progress messages
-	{
-		"j-hui/fidget.nvim",
-		branch = "legacy",
-		enabled = true,
-		config = function()
-			require("fidget").setup({
-				window = { blend = 0 },
-			})
-		end,
-	},
-
-
-	{
-		'echasnovski/mini.nvim',
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require('mini.ai').setup { n_lines = 500 }
-
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			require('mini.surround').setup()
-
-			-- Simple and easy statusline.
-			--  You could remove this setup call if you don't like it,
-			--  and try some other statusline plugin
-			local statusline = require 'mini.statusline'
-			-- set use_icons to true if you have a Nerd Font
-			statusline.setup { use_icons = vim.g.have_nerd_font }
-
-			-- You can configure sections in the statusline by overriding their
-			-- default behavior. For example, here we set the section for
-			-- cursor location to LINE:COLUMN
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return '%2l:%-2v'
-			end
-		end,
 	},
 }
