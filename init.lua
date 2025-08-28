fa1424e69611ca37839b766fda4a5e36a951a582
raivokinne <raivokinne236@gmail.com>
2025-08-28 22:49:03 +0300

    update

--- init.lua
+++ init.lua
@@ -1,63 +1,110 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.guicursor = ""
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
-
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
+-- vim.opt.cursorline = true
 
-vim.keymap.set('n', '-', vim.cmd.Ex)
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('n', 'tt', ':tabnew<CR>')
-vim.keymap.set('n', '<S-l>', ':tabnext<CR>')
-vim.keymap.set('n', '<S-h>', ':tabprevious<CR>')
-vim.keymap.set('n', 'tc', ':tabclose<CR>')
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set({ 'n', 'v' }, '<leader>Y', [["+Y]])
-vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww ~/.local/bin/tmux-sessionizer<CR>')
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
-
+vim.keymap.set("n", "-", vim.cmd.Ex)
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "tt", ":tabnew<CR>")
+vim.keymap.set("n", "<S-l>", ":tabnext<CR>")
+vim.keymap.set("n", "<S-h>", ":tabprevious<CR>")
+vim.keymap.set("n", "tc", ":tabclose<CR>")
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww ~/.local/bin/tmux-sessionizer<CR>")
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 local augroup = vim.api.nvim_create_augroup
-local RaivoGroup = augroup('Raivo', {})
+local RaivoGroup = augroup("Raivo", {})
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup("HighlightYank", {})
+
+autocmd("TextYankPost", {
+	group = yank_group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 40,
+		})
+	end,
+})
+
+autocmd({ "BufWritePre" }, {
+	group = RaivoGroup,
+	pattern = "*",
+	command = [[%s/\s\+$//e]],
+})
+
+autocmd("LspAttach", {
+	group = RaivoGroup,
+	callback = function(e)
+		local opts = { buffer = e.buf }
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, opts)
+		vim.keymap.set("n", "K", function()
+			vim.lsp.buf.hover()
+		end, opts)
+		vim.keymap.set("n", "<leader>ws", function()
+			vim.lsp.buf.workspace_symbol()
+		end, opts)
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, opts)
+		vim.keymap.set("n", "<leader>ca", function()
+			vim.lsp.buf.code_action()
+		end, opts)
+		vim.keymap.set("n", "<leader>rr", function()
+			vim.lsp.buf.references()
+		end, opts)
+		vim.keymap.set("n", "<leader>rn", function()
+			vim.lsp.buf.rename()
+		end, opts)
+		vim.keymap.set("i", "<C-h>", function()
+			vim.lsp.buf.signature_help()
+		end, opts)
+		vim.keymap.set("n", "[d", function()
+			vim.diagnostic.goto_next()
+		end, opts)
+		vim.keymap.set("n", "]d", function()
+			vim.diagnostic.goto_prev()
+		end, opts)
+	end,
+})
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-	local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 	if vim.v.shell_error ~= 0 then
-		error('Error cloning lazy.nvim:\n' .. out)
+		error("Error cloning lazy.nvim:\n" .. out)
 	end
 end
 
@@ -65,302 +112,254 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup({
+require("lazy").setup({
 	{
-		'nvim-telescope/telescope.nvim',
+		"nvim-telescope/telescope.nvim",
 		dependencies = {
-			'nvim-lua/plenary.nvim',
+			"nvim-lua/plenary.nvim",
 		},
 		config = function()
-			local builtin = require 'telescope.builtin'
-			vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-			vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-			vim.keymap.set('n', '<leader>ps', function()
-				builtin.live_grep {
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+			vim.keymap.set("n", "<leader>ps", function()
+				builtin.live_grep({
 					grep_open_files = true,
-					prompt_title = 'Live Grep in Open Files',
-				}
-			end, { desc = '[S]earch [/] in Open Files' })
+					prompt_title = "Live Grep in Open Files",
+				})
+			end, { desc = "[S]earch [/] in Open Files" })
 		end,
 	},
 	{
-		'neovim/nvim-lspconfig',
+		"neovim/nvim-lspconfig",
 		dependencies = {
-			{ 'mason-org/mason.nvim', opts = {} },
-			'mason-org/mason-lspconfig.nvim',
-			'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-			{ 'j-hui/fidget.nvim', opts = {} },
-
-			'saghen/blink.cmp',
+			"stevearc/conform.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/nvim-cmp",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"j-hui/fidget.nvim",
 		},
 		config = function()
-			vim.api.nvim_create_autocmd('LspAttach', {
-				group = RaivoGroup,
-				callback = function(e)
-					local opts = { buffer = e.buf }
-					vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-					vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-					vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
-					vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-					vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-					vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-					vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-					vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-					vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-					vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+			require("conform").setup({
+				notify_on_error = false,
+				format_on_save = function(bufnr)
+					local disable_filetypes = { c = true, cpp = true }
+					if disable_filetypes[vim.bo[bufnr].filetype] then
+						return nil
+					else
+						return {
+							timeout_ms = 500,
+							lsp_format = "fallback",
+						}
+					end
 				end,
+				formatters_by_ft = {
+					lua = { "stylua" },
+					go = { "gofmt" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					elixir = { "mix" },
+				},
 			})
+			local cmp = require("cmp")
+			local cmp_lsp = require("cmp_nvim_lsp")
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				{},
+				vim.lsp.protocol.make_client_capabilities(),
+				cmp_lsp.default_capabilities()
+			)
 
-			local capabilities = require('blink.cmp').get_lsp_capabilities()
+			require("fidget").setup({})
+			require("mason").setup()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"rust_analyzer",
+					"gopls",
+					"tailwindcss",
+				},
+				handlers = {
+					function(server_name) -- default handler (optional)
+						require("lspconfig")[server_name].setup({
+							capabilities = capabilities,
+						})
+					end,
 
-			local lspconfig = require 'lspconfig'
-
-			local servers = {
-				zls = {},
-				bashls = true,
-				gopls = {
-					manual_install = true,
-					settings = {
-						gopls = {
-							hints = {
-								assignVariableTypes = true,
-								compositeLiteralFields = true,
-								compositeLiteralTypes = true,
-								constantValues = true,
-								functionTypeParameters = true,
-								parameterNames = true,
-								rangeVariableTypes = true,
-							},
-						},
-					},
-				},
-				lua_ls = {
-					cmd = { 'lua-language-server' },
-				},
-				rust_analyzer = true,
-				svelte = true,
-				templ = true,
-				taplo = true,
-				intelephense = {
-					settings = {
-						intelephense = {
-							format = {
-								braces = 'k&r',
-							},
-						},
-					},
-				},
-				pyright = true,
-				vtsls = {
-					server_capabilities = {
-						documentFormattingProvider = false,
-					},
-				},
-				-- denols = true,
-				jsonls = {
-					server_capabilities = {
-						documentFormattingProvider = false,
-					},
-				},
-				yamlls = {
-					settings = {
-						yaml = {
-							schemaStore = {
-								enable = false,
-								url = '',
-							},
-						},
-					},
-				},
-				ols = {},
-				racket_langserver = { manual_install = true },
-				roc_ls = { manual_install = true },
-				ocamllsp = {
-					manual_install = true,
-					cmd = { 'ocamllsp' },
-					settings = {
-						codelens = { enable = true },
-						inlayHints = { enable = true },
-						syntaxDocumentation = { enable = true },
-					},
-
-					server_capabilities = { semanticTokensProvider = false },
-				},
-				elixirls = {
-					cmd = { '/home/tjdevries/.local/share/nvim/mason/bin/elixir-ls' },
-					root_dir = require('lspconfig.util').root_pattern { 'mix.exs' },
-				},
-				clangd = {
-					manual_install = true,
-					filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
-				},
-
-				tailwindcss = {
-					init_options = {
-						userLanguages = {
-							elixir = 'phoenix-heex',
-							eruby = 'erb',
-							heex = 'phoenix-heex',
-						},
-					},
-					settings = {
-						tailwindCSS = {
-							experimental = {
-								classRegex = {
-									[[class: "([^"]*)]],
-									[[className="([^"]*)]],
+					zls = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.zls.setup({
+							root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+							settings = {
+								zls = {
+									enable_inlay_hints = true,
+									enable_snippets = true,
+									warn_style = true,
 								},
 							},
-						},
-					},
+						})
+						vim.g.zig_fmt_parse_errors = 0
+						vim.g.zig_fmt_autosave = 0
+					end,
+					["lua_ls"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.lua_ls.setup({
+							capabilities = capabilities,
+							settings = {
+								Lua = {
+									format = {
+										enable = true,
+										-- Put format options here
+										-- NOTE: the value should be STRING!!
+										defaultConfig = {
+											indent_style = "space",
+											indent_size = "2",
+										},
+									},
+								},
+							},
+						})
+					end,
+					["tailwindcss"] = function()
+						local lspconfig = require("lspconfig")
+						lspconfig.tailwindcss.setup({
+							capabilities = capabilities,
+							filetypes = {
+								"html",
+								"css",
+								"scss",
+								"javascript",
+								"javascriptreact",
+								"typescript",
+								"typescriptreact",
+								"vue",
+								"svelte",
+							},
+							settings = {
+								tailwindCSS = {
+									experimental = {
+										classRegex = {
+											"tw`([^`]*)",
+											'tw="([^"]*)',
+											'tw={"([^"}]*)',
+											"tw\\.\\w+`([^`]*)",
+											"tw\\(.*?\\)`([^`]*)",
+										},
+									},
+								},
+							},
+						})
+					end,
 				},
-			}
+			})
 
-			local servers_to_install = vim.tbl_filter(function(key)
-				local t = servers[key]
-				if type(t) == 'table' then
-					return not t.manual_install
-				else
-					return t
-				end
-			end, vim.tbl_keys(servers))
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-			require('mason').setup()
-			local ensure_installed = {
-				'stylua',
-				'lua_ls',
-				'delve',
-				-- "tailwind-language-server",
-			}
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-j>"] = cmp.mapping.select_prev_item(cmp_select),
+					["<C-k>"] = cmp.mapping.select_next_item(cmp_select),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+				}, {
+					{ name = "buffer" },
+				}),
+			})
 
-			vim.list_extend(ensure_installed, servers_to_install)
-			require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
-			for name, config in pairs(servers) do
-				if config == true then
-					config = {}
-				end
-				config = vim.tbl_deep_extend('force', {}, {
-					capabilities = capabilities,
-				}, config)
-
-				lspconfig[name].setup(config)
-			end
+			vim.diagnostic.config({
+				-- update_in_insert = true,
+				float = {
+					focusable = false,
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			})
 		end,
 	},
-
-	{ -- Autocompletion
-		'saghen/blink.cmp',
-		event = 'VimEnter',
-		version = '1.*',
-		dependencies = {
-			{
-				'L3MON4D3/LuaSnip',
-				version = '2.*',
-				build = (function()
-					if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-						return
-					end
-					return 'make install_jsregexp'
-				end)(),
-				dependencies = {
-					{
-						'rafamadriz/friendly-snippets',
-						config = function()
-							require('luasnip.loaders.from_vscode').lazy_load()
-						end,
-					},
-				},
-				opts = {},
-			},
-		},
-		--- @module 'blink.cmp'
-		--- @type blink.cmp.Config
-		opts = {
-			keymap = {
-				['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-				['<C-e>'] = { 'hide' },
-				['<C-y>'] = { 'select_and_accept' },
-
-				['<Up>'] = { 'select_prev', 'fallback' },
-				['<Down>'] = { 'select_next', 'fallback' },
-				['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
-				['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
-
-				['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
-				['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
-
-				['<Tab>'] = { 'snippet_forward', 'fallback' },
-				['<S-Tab>'] = { 'snippet_backward', 'fallback' },
-			},
-
-			appearance = {
-				nerd_font_variant = 'mono',
-			},
-
-			completion = {
-				documentation = { auto_show = true, auto_show_delay_ms = 500 },
-			},
-
-			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer' },
-			},
-
-			snippets = { preset = 'luasnip' },
-			fuzzy = { implementation = 'lua' },
-			signature = { enabled = true },
-		},
-	},
 	{
-		"vague2k/vague.nvim",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other plugins
-		config = function()
-			require("vague").setup({
-				transparent = true, -- don't set background
-				-- disable bold/italic globally in `style`
-				bold = true,
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			styles = {
 				italic = false,
-			})
-			vim.cmd("colorscheme vague")
-		end
+				bold = true,
+				transparency = true,
+			},
+		},
+		config = function(_, opts)
+			require("rose-pine").setup(opts)
+		end,
+		init = function()
+			vim.cmd.colorscheme("rose-pine")
+		end,
 	},
 	{ -- Highlight, edit, and navigate code
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
-		main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		main = "nvim-treesitter.configs", -- Sets main module to use for opts
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 		opts = {
-			ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+			ensure_installed = {
+				"bash",
+				"c",
+				"diff",
+				"html",
+				"lua",
+				"luadoc",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"vim",
+				"vimdoc",
+			},
 			-- Autoinstall languages that are not installed
 			auto_install = true,
 			highlight = {
 				enable = true,
-				additional_vim_regex_highlighting = { 'ruby' },
+				additional_vim_regex_highlighting = { "ruby" },
 			},
-			indent = { enable = true, disable = { 'ruby' } },
+			indent = { enable = true, disable = { "ruby" } },
 		},
 	},
 	{
-		'folke/trouble.nvim',
+		"folke/trouble.nvim",
 		opts = {},
-		cmd = 'Trouble',
+		cmd = "Trouble",
 		keys = {
 			{
-				'<leader>xx',
-				'<cmd>Trouble diagnostics toggle<cr>',
-				desc = 'Diagnostics (Trouble)',
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
 			},
 			{
-				'[t',
-				'<cmd>Trouble prev<cr>',
-				desc = 'Previous (Trouble)',
+				"[t",
+				"<cmd>Trouble prev<cr>",
+				desc = "Previous (Trouble)",
 			},
 			{
-				']t',
-				'<cmd>Trouble next<cr>',
-				desc = 'Next (Trouble)',
+				"]t",
+				"<cmd>Trouble next<cr>",
+				desc = "Next (Trouble)",
 			},
 		},
 	},
